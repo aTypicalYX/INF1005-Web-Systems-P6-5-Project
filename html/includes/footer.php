1517cb630e66e099c60aae77f8d8109228671547
aTypicalYX <dooyanxu@gmail.com>
2026-03-11 15:59:36 +0800

    added back the bootstrap script tag in footer.php, which was accidentally removed in a previous commit. This ensures that the Bootstrap JavaScript functionalities work properly on the website.

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -40,6 +40,6 @@
         </div>
     </footer>
 
-    <?= isset($extraScripts) ? $extraScripts : '' ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>    <?= isset($extraScripts) ? $extraScripts : '' ?>
 </body>
 </html>
